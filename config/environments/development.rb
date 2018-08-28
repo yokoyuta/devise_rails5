1b6475bd6e81227d926a464348415fb97e8f97c0
yokoyuta <military_phantom_ship_no99@yahoo.co.jp>
2018-08-28 18:14:50 +0900

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # mailer setting
+  # default url
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "yutakay40@gmail.com",
+    :password => "torajiro4649",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
